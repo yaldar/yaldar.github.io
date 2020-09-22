3f5b1eb61ed67ebeb8b22da9d889bce0d4257e92
Yaldar <yaldar.hapak@gmail.com>
2020-09-22 03:35:10 +0200

    Updates

---
+++ precache-manifest.482eb5516ad91db5f7be1756ddae2cda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0f8c9f814a640190ae3bbd1065667a",
+    "url": "/yaldar.github.io/index.html"
+  },
+  {
+    "revision": "15bbff03891fecdd74ee",
+    "url": "/yaldar.github.io/static/css/main.26e13a99.chunk.css"
+  },
+  {
+    "revision": "2ec8233f689d1170e921",
+    "url": "/yaldar.github.io/static/js/2.6da1e238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yaldar.github.io/static/js/2.6da1e238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bbff03891fecdd74ee",
+    "url": "/yaldar.github.io/static/js/main.5a91b5cf.chunk.js"
+  },
+  {
+    "revision": "2f0304ea336d6830253e",
+    "url": "/yaldar.github.io/static/js/runtime-main.0a95d86f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yaldar.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
