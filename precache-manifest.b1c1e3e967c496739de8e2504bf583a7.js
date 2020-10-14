493c8dd999123420896e622a6c1a454bfaedf509
yaldar <yaldar.hapak@gmail.com>
2020-10-14 22:26:25 +0200

    Updates

---
+++ precache-manifest.b1c1e3e967c496739de8e2504bf583a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5934ad60efc777f50a4ebe7d2eb4d5fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48193ce270d5830faeba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7357af019cfbac6eb3cf",
+    "url": "/static/js/2.9e7789ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9e7789ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48193ce270d5830faeba",
+    "url": "/static/js/main.62546e94.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yaldar.github.io/precache-manifest.482eb5516ad91db5f7be1756ddae2cda.js"
+  "/precache-manifest.b1c1e3e967c496739de8e2504bf583a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yaldar.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
