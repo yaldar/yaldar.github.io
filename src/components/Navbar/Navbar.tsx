cc611e5a88e2a14688e323de3221145618c5b788
yaldar <yaldar.hapak@gmail.com>
2020-11-06 11:35:42 +0100

    --added funtionality to collapse menue on scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Icon } from 'semantic-ui-react';
 import NavbarLink from '../NavbarLink/NavbarLink';
 import './Navbar.css';
 
 const Navbar = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const toggleCollapsed = () => setCollapsed((prev) => !prev);
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpanded = () => setExpanded((prev) => !prev);
+  const r = useRef<Icon>(null);
+  const collapseOnScroll = () => {
+    setExpanded(false);
+  };
+  const collapseOnClick = (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    if (target.id && target.id !== 'icon') toggleExpanded();
+  };
 
+  useEffect(() => {
+    document.addEventListener('scroll', collapseOnScroll);
+    document.addEventListener('click', collapseOnClick);
+    return () => {
+      document.removeEventListener('scroll', collapseOnScroll);
+      document.removeEventListener('click', collapseOnClick);
+    };
+  }, []);
   return (
-    <nav className={`navbar ${collapsed ? 'collapsed' : ''}`} id="navbar">
-      <Icon name="bars" className="icon" onClick={toggleCollapsed} size="big" />
+    <nav className={`navbar ${expanded ? 'expanded' : ''}`} id="navbar">
+      <Icon name="bars" className="icon" onClick={toggleExpanded} size="big" ref={r} />
       <NavbarLink to="About" />
       <NavbarLink to="Experience" />
       <NavbarLink to="Skills" />
