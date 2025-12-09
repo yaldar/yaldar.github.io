d01c4cf4358ac827d4abd167a7bbff1fca929a62
yaldar hapak <yaldar.hapak@refined.com>
2025-12-09 22:03:28 +0100

    formatted the code

--- src/components/SkillList/SkillList.tsx
+++ src/components/SkillList/SkillList.tsx
@@ -1,35 +1,34 @@
 /* eslint-disable react/no-array-index-key */
 // @ts-nocheck
-import React, { useState } from 'react';
-import { Icon } from 'semantic-ui-react';
-import { SkillListType } from '../../types';
-import { iconsArray } from '../../data/skillsData';
+import React from 'react';
+import {Icon} from 'semantic-ui-react';
+import {SkillListType} from '../../types';
+import {iconsArray} from '../../data/skillsData';
 import './SkillList.css';
 
 // TODO fix ugly code plz look away
-const SkillList = ({ title, list }: SkillListType) => {
-  const normalize = (name: string) => {
-    const t = name.replace(/[-./]/g, '');
-    return t;
-  };
-  return (
-    <ul className={`${title} skill-list`}>
-      <h3>{title}</h3>
-      {list.map((el, i) => {
-        return (
-          <div className="skill">
-            {el.semanticIcon ? (
-              <Icon name={el.semanticIcon} />
-            ) : (
-              <img src={iconsArray[normalize(el.name)]} className="icon" />
-            )}
+const SkillList = ({title, list}: SkillListType) => {
+    const normalize = (name: string) => {
+        return name.replace(/[-./]/g, '');
+    };
+    return (
+        <ul className={`${title} skill-list`}>
+            <h3>{title}</h3>
+            {list.map((el, i) => {
+                return (
+                    <div className="skill">
+                        {el.semanticIcon ? (
+                            <Icon name={el.semanticIcon}/>
+                        ) : (
+                            <img src={iconsArray[normalize(el.name)]} className="icon" alt={el.semanticIcon}/>
+                        )}
 
-            <li key={i}>{el.name}</li>
-          </div>
-        );
-      })}
-    </ul>
-  );
+                        <li key={i}>{el.name}</li>
+                    </div>
+                );
+            })}
+        </ul>
+    );
 };
 
 export default SkillList;
