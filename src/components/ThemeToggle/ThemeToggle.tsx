a7751d87390e2793d0167a839d2106d50ade5915
yaldar hapak <yaldar.hapak@refined.com>
2026-01-27 22:26:17 +0100

    bumped react-router-dom version

---
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useTheme } from '@mui/material/styles';
+
+interface ThemeToggleProps {
+  toggleColorMode: () => void;
+}
+
+const ThemeToggle: React.FC<ThemeToggleProps> = ({ toggleColorMode }) => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        borderRadius: 1,
+        p: 1,
+      }}
+    >
+      <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
+        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+    </Box>
+  );
+};
+
+export default ThemeToggle;
